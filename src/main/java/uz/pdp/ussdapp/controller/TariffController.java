ce148530433572b7094103357f14420575cb108d
Mukhammadqodir <realkings1007.com>
2021-10-26 18:31:33 +0300

    changed TariffController

--- src/main/java/uz/pdp/ussdapp/controller/TariffController.java
+++ src/main/java/uz/pdp/ussdapp/controller/TariffController.java
@@ -4,14 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.ussdapp.entity.Tariff;
 import uz.pdp.ussdapp.payload.ApiResponse;
 import uz.pdp.ussdapp.payload.TariffDTO;
 import uz.pdp.ussdapp.repository.TariffRepository;
 import uz.pdp.ussdapp.service.TariffService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tariff")
@@ -28,4 +29,41 @@ public class TariffController {
 
         return ResponseEntity.status(response.isSuccess() ? 201 : 409).body(response);
     }
+
+    //get all
+    @GetMapping
+    public List<Tariff> getTariff(){
+        List<Tariff> all = tariffRepository.findAll();
+        return all;
+
+    }
+
+    //byname
+    @GetMapping
+    public Optional<Tariff> getTarif(@PathVariable String name){
+        boolean existsByName = tariffRepository.existsByName(name);
+        if (!existsByName) return Optional.empty();
+        return tariffRepository.findByName(name);
+
+    }
+
+    @PutMapping
+    public HttpEntity<?>editTariff(@PathVariable String name,@RequestBody TariffDTO   tariffDTO){
+        ApiResponse apiResponse = tariffService.editTariff(name, tariffDTO);
+        return ResponseEntity.status(apiResponse.isSuccess()?201:409).body(apiResponse);
+    }
+
+    //delete by name
+    @DeleteMapping
+    public HttpEntity<?>deleteById(@PathVariable String name){
+
+        if(!tariffRepository.existsByName(name)) return null;
+
+        tariffRepository.deleteByName(name);
+
+        return  ResponseEntity.ok().body("deleted");
+
+
+    }
+
 }
--- src/main/java/uz/pdp/ussdapp/repository/TariffRepository.java
+++ src/main/java/uz/pdp/ussdapp/repository/TariffRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.pdp.ussdapp.entity.Tariff;
 import uz.pdp.ussdapp.entity.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TariffRepository extends JpaRepository<Tariff, Integer> {
     boolean existsByName(String name);
+
+    Optional<Tariff>findByName(String name);
+
+    void deleteByName(String name);
 }
