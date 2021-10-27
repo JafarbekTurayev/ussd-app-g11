3feb9b6ff7444614cfd8526278ca943ecd36c1a7
shaxzod <ahrorisaev73@gmail.com>
2021-10-27 09:05:48 +0500

    Update payment and dashboard

--- src/main/java/uz/pdp/ussdapp/repository/SimcardRepository.java
+++ src/main/java/uz/pdp/ussdapp/repository/SimcardRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.pdp.ussdapp.entity.SimCard;
 import uz.pdp.ussdapp.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,5 +13,6 @@ public interface SimcardRepository extends JpaRepository<SimCard, UUID> {
     boolean existsByCodeAndNumber(String code, String number);
 
     Optional<SimCard> findByCodeAndNumber(String code, String number);
+    List<SimCard> findAllByActive(boolean active);
 
 }
--- src/main/java/uz/pdp/ussdapp/repository/TariffRepository.java
+++ src/main/java/uz/pdp/ussdapp/repository/TariffRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.pdp.ussdapp.entity.Tariff;
 import uz.pdp.ussdapp.entity.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TariffRepository extends JpaRepository<Tariff, Integer> {
     boolean existsByName(String name);
+
 }
